bfa326dbf5c4c85eeee6fa358f72e96158d17af6
PRAJNESH N <94782333+prajneshn@users.noreply.github.com>
2022-09-20 19:50:34 +0530

    final

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class="foot w3-container" style="height: 3vh !important;width:100%;color:#35549b;background-color:#fff;font-weight:bolder;margin:0;margin-top:1vw;bottom:0">
+    <center>&nbsp;&nbsp;Copyright&copy;:2022</center>
+</div>
